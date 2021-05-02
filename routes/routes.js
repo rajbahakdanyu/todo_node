97f16f1b2e0d7f2f070df07144fc934bb8a459ec
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-05-02 18:27:30 +0545

    Added delete function

--- routes/routes.js
+++ routes/routes.js
@@ -6,47 +6,59 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
 	sqlcon.query("SELECT * FROM task", (err, rows, feilds) => {
-        if (!err) {
-            res.render('main', { tasks: rows });
-        } else {
-            console.log(err);
-        }
-    });
+		if (!err) {
+			res.render("main", { tasks: rows });
+		} else {
+			console.log(err);
+		}
+	});
 });
 
 router.post("/", (req, res) => {
-    var querystring = `INSERT INTO task (title) VALUES ('${req.body.title}')`;
+	var querystring = `INSERT INTO task (title) VALUES ('${req.body.title}')`;
 
 	sqlcon.query(querystring, (err, rows, feilds) => {
-        if (!err) {
-            res.redirect('/');
-        } else {
-            console.log(err);
-        }
-    });
+		if (!err) {
+			res.redirect("/");
+		} else {
+			console.log(err);
+		}
+	});
 });
 
-router.get('/update/:id', (req, res) => {
-    var querystring = `SELECT * FROM task WHERE id = ${req.params.id}`;
+router.get("/update/:id", (req, res) => {
+	var querystring = `SELECT * FROM task WHERE id = ${req.params.id}`;
 
 	sqlcon.query(querystring, (err, rows, feilds) => {
-        if (!err) {
-            if (rows[0].completed == 0) {
-                querystring = `UPDATE task SET completed = 1 WHERE id = ${req.params.id}`;
-            } else {
-                querystring = `UPDATE task SET completed = 0 WHERE id = ${req.params.id}`;
-            }
-            sqlcon.query(querystring, (err, rows, feilds) => {
-                if (!err) {                    
-                    res.redirect('/');
-                } else {
-                    console.log(err);
-                }
-            });
-        } else {
-            console.log(err);
-        }
-    });
-})
+		if (!err) {
+			if (rows[0].completed == 0) {
+				querystring = `UPDATE task SET completed = 1 WHERE id = ${req.params.id}`;
+			} else {
+				querystring = `UPDATE task SET completed = 0 WHERE id = ${req.params.id}`;
+			}
+			sqlcon.query(querystring, (err, rows, feilds) => {
+				if (!err) {
+					res.redirect("/");
+				} else {
+					console.log(err);
+				}
+			});
+		} else {
+			console.log(err);
+		}
+	});
+});
+
+router.get("/delete/:id", (req, res) => {
+	var querystring = `DELETE FROM task WHERE id = ${req.params.id}`;
+
+	sqlcon.query(querystring, (err, rows, feilds) => {
+		if (!err) {
+			res.redirect("/");
+		} else {
+			console.log(err);
+		}
+	});
+});
 
 module.exports = router;
